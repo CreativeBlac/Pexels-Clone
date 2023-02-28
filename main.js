d5db9ae0a0c050743737adbd3f64ca6af3c6a37d
CreativeBlac <alexanderofori07@gmail.com>
2023-02-28 11:02:43 +0000

    trying to build a clone of pexels.com with pexels api that is integrated into pexels and one can download pictures

---
+++ main.js
@@ -0,0 +1,16 @@
+const apiKey = 'jcZGeGMCLey8jUuI08tKEL3XPTb2RGDd4HxFsxc8tmSs7FBvgDKNFBWN';
+
+async function getImg(apiKey) {
+    const response = await fetch(`https://api.pexels.com/v1/search?query=people`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'Authorization': apiKey
+        }
+    })
+    const json = await response.json();
+    return json;
+}
+
+const rep = getImg();
+console.log()
