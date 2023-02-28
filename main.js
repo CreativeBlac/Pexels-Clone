9aee4e585c208ebf46f45f3c71694d12e98eb9ab
CreativeBlac <alexanderofori07@gmail.com>
2023-02-28 16:14:02 +0000

    working pexels aoi fetched from pecels.com

--- main.js
+++ main.js
@@ -2,14 +2,30 @@ const apiKey = 'jcZGeGMCLey8jUuI08tKEL3XPTb2RGDd4HxFsxc8tmSs7FBvgDKNFBWN';
 
 async function getImg(apiKey) {
     const response = await fetch(`https://api.pexels.com/v1/search?query=people`, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-            'Authorization': apiKey
-        }
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Authorization': apiKey
+            }
+        })
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            getPhotos(data.photos)
+        })
+}
+getImg()
+
+function getPhotos(photos) {
+    photos.map(photo => {
+        const openEl = document.querySelector('.open');
+        const img = document.createElement('img')
+        img.src = photo.src.large
+        openEl.appendChild(img)
     })
-    const json = await response.json();
-    return json;
 }
 
-getImg().then(response => console.log(response))
+// getImg().then(response => console.log(response.photos)).catch(err => {
+//     console.log('errors occured at:' + err.message)
+// })
