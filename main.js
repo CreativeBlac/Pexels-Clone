a043c33dacecabb2f80fcd85de704b71d0549835
CreativeBlac <alexanderofori07@gmail.com>
2023-02-28 20:38:57 +0000

    revised version of pexels working and fetching photos

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const apiKey = 'jcZGeGMCLey8jUuI08tKEL3XPTb2RGDd4HxFsxc8tmSs7FBvgDKNFBWN';
 
 async function getImg(apiKey) {
-    const response = await fetch(`https://api.pexels.com/v1/search?query=people`, {
+    const response = await fetch("https://api.pexels.com/v1/search?query=people", {
             method: 'GET',
             mode: 'cors',
             headers: {
                 'Authorization': apiKey
+
             }
         })
         .then(response => {
@@ -13,6 +14,8 @@ async function getImg(apiKey) {
         })
         .then(data => {
             getPhotos(data.photos)
+        }).catch((err) => {
+            console.log('Error Occured At:' + err.message);
         })
 }
 getImg()
@@ -20,12 +23,8 @@ getImg()
 function getPhotos(photos) {
     photos.map(photo => {
         const openEl = document.querySelector('.open');
-        const img = document.createElement('img')
-        img.src = photo.src.large
+        const img = document.createElement('img');
+        img.src = photo.src.original
         openEl.appendChild(img)
     })
 }
-
-// getImg().then(response => console.log(response.photos)).catch(err => {
-//     console.log('errors occured at:' + err.message)
-// })
